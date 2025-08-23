58063d31270a1c5095e02fec493ca865ff39eead
Atul Rathod <atulrathodc@gmail.com>
2025-08-23 11:55:43 +0530

    updated property files

---
+++ git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COMMIT_MESSAGE="updated property files"
+
+# Add all changes
+git add .
+
+# Commit with the provided message
+git commit -m "$COMMIT_MESSAGE"
+
+# Push to the current branch
+git push
+
+echo "Changes pushed successfully!"
